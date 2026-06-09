5743bf568b123a25c29f88a082e0f62d3f5a0728
Hodaya Guma <hodyaju@gmail.com>
2026-06-09 11:26:36 +0300

    .

--- src/ai/ui/Circle.java
+++ src/ai/ui/Circle.java
@@ -1,23 +1,16 @@
 package ai.ui;
 
-<<<<<<< HEAD
-public class Circle {
-    public int cx, cy, radius;
-=======
 import java.awt.Color;
 
 public class Circle {
     public int cx, cy, radius;
     public Color color;
->>>>>>> b943467914e9b689d0b17c128dfc9d781a9c7350
     public boolean isBlinking;
 
     public Circle(int cx, int cy, int radius) {
         this.cx = cx;
         this.cy = cy;
         this.radius = radius;
-<<<<<<< HEAD
-=======
         this.color = Color.BLACK;
         this.isBlinking = false;
     }
@@ -27,7 +20,6 @@ public class Circle {
         this.cy = cy;
         this.radius = radius;
         this.color = color;
->>>>>>> b943467914e9b689d0b17c128dfc9d781a9c7350
         this.isBlinking = false;
     }
 
@@ -36,11 +28,8 @@ public class Circle {
         this.cy = cy;
         this.radius = radius;
     }
-<<<<<<< HEAD
-=======
 
     public void setColor(Color color) {
         this.color = color;
     }
->>>>>>> b943467914e9b689d0b17c128dfc9d781a9c7350
 }
